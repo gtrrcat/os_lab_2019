29bf9f3e07aaaab47d496cb4c540d95412fbce01
gtrrcat <161066972+gtrrcat@users.noreply.github.com>
2025-11-25 16:15:56 +0000

    лаб7 2025

--- lab7/src/udpclient.c
+++ lab7/src/udpclient.c
@@ -1,7 +1,6 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <arpa/inet.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -15,43 +14,45 @@
 #define SLEN sizeof(struct sockaddr_in)
 
 int main(int argc, char **argv) {
-  int sockfd, n;
-  char sendline[BUFSIZE], recvline[BUFSIZE + 1];
-  struct sockaddr_in servaddr;
-  struct sockaddr_in cliaddr;
+    int sockfd, n;
+    char sendline[BUFSIZE], recvline[BUFSIZE + 1];
+    struct sockaddr_in servaddr;
 
-  if (argc != 2) {
-    printf("usage: client <IPaddress of server>\n");
-    exit(1);
-  }
-
-  memset(&servaddr, 0, sizeof(servaddr));
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_port = htons(SERV_PORT);
-
-  if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) < 0) {
-    perror("inet_pton problem");
-    exit(1);
-  }
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-    perror("socket problem");
-    exit(1);
-  }
-
-  write(1, "Enter string\n", 13);
-
-  while ((n = read(0, sendline, BUFSIZE)) > 0) {
-    if (sendto(sockfd, sendline, n, 0, (SADDR *)&servaddr, SLEN) == -1) {
-      perror("sendto problem");
-      exit(1);
+    if (argc != 2) {
+        printf("usage: client <IPaddress of server>\n");
+        exit(1);
     }
 
-    if (recvfrom(sockfd, recvline, BUFSIZE, 0, NULL, NULL) == -1) {
-      perror("recvfrom problem");
-      exit(1);
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERV_PORT);
+
+    if (inet_pton(AF_INET, argv[1], &servaddr.sin_addr) < 0) {
+        perror("inet_pton problem");
+        exit(1);
+    }
+    
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket problem");
+        exit(1);
     }
 
-    printf("REPLY FROM SERVER= %s\n", recvline);
-  }
-  close(sockfd);
+    write(1, "Enter string\n", 13);
+
+    while ((n = read(0, sendline, BUFSIZE)) > 0) {
+        if (sendto(sockfd, sendline, n, 0, (SADDR *)&servaddr, SLEN) == -1) {
+            perror("sendto problem");
+            exit(1);
+        }
+
+        if (recvfrom(sockfd, recvline, BUFSIZE, 0, NULL, NULL) == -1) {
+            perror("recvfrom problem");
+            exit(1);
+        }
+
+        printf("REPLY FROM SERVER= %s\n", recvline);
+    }
+    
+    close(sockfd);
+    return 0;
 }
